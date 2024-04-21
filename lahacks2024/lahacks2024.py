afefbb8951caa8e79585cdde23422f3a7b01c155
g1oom <albertowj2001@gmail.com>
2024-04-20 20:01:52 -0700

    Add postgres remote database and integrate with matching module

--- lahacks2024/lahacks2024.py
+++ lahacks2024/lahacks2024.py
@@ -2,13 +2,254 @@
 
 # Import all the pages.
 from lahacks2024.pages import *
+from lahacks2024.templates import template
 
+from sqlmodel import select, Field
 import reflex as rx
 
+class User(rx.Model, table=True):
+    username: str = Field(primary_key=True)
+    name: str
+    age: int
+    illness: str
+    language: str
 
 class State(rx.State):
-    """Define empty state to allow access to rx.State.router."""
+    username: str
+    name: str
+    age: int
+    illness: str
+    language: str
+    users: list[User] = []
+    sort_value: str = ""
+    num_users: int
 
+    def load_entries(self) -> list[User]:
+        """Get all users from the database."""
+        with rx.session() as session:
+            self.users = session.exec(
+                User.select().where(
+                    User.illness.contains('ill')
+                )
+            ).all()
+
+            self.num_users = len(self.users)
+
+            if self.sort_value:
+                self.users = sorted(
+                    self.users, key=lambda user: getattr(user, self.sort_value)
+                )
+
+    def sort_values(self, sort_value: str):
+        self.sort_value = sort_value
+        self.load_entries()
+
+    def add_user(self):
+        """Add a user to the database."""
+        with rx.session() as session:
+            if session.exec(
+                select(User).where(User.username == self.username)
+            ).first():
+                return rx.window_alert("User already exists")
+            session.add(
+                User(
+                    username=self.username,
+                    name=self.name,
+                    age=self.age,
+                    illness=self.illness,
+                    language=self.language
+                )
+            )
+            session.commit()
+        self.load_entries()
+        return rx.window_alert(f"User {self.username} has been added.")
+    
+    def on_load(self):
+        self.load_entries()
+
+def show_user(user: User):
+    """Show a user in a table row."""
+    return rx.table.row(
+        rx.table.cell(user.username),
+        rx.table.cell(user.name),
+        rx.table.cell(user.age),
+        rx.table.cell(user.illness),
+        rx.table.cell(user.language)
+    )
+
+def add_user():
+    return rx.dialog.root(
+        rx.dialog.trigger(
+            rx.button(
+                rx.flex(
+                    "Add New User",
+                    rx.icon(tag="plus", width=24, height=24),
+                    spacing="3",
+                ),
+                size="4",
+                radius="full",
+            ),
+        ),
+        rx.dialog.content(
+            rx.dialog.title(
+                "User Details",
+                font_family="Inter",
+            ),
+            rx.dialog.description(
+                "Add your user profile details.",
+                size="2",
+                mb="4",
+                padding_bottom="1em",
+            ),
+            rx.flex(
+                rx.text(
+                    "Username",
+                    as_="div",
+                    size="2",
+                    mb="1",
+                    weight="bold",
+                ),
+                rx.input(placeholder="Username", on_blur=State.set_username),
+                rx.text(
+                    "Name",
+                    as_="div",
+                    size="2",
+                    mb="1",
+                    weight="bold",
+                ),
+                rx.input(placeholder="Name", on_blur=State.set_name),
+                rx.text(
+                    "Age",
+                    as_="div",
+                    size="2",
+                    mb="1",
+                    weight="bold",
+                ),
+                rx.input(placeholder="Age", on_blur=State.set_age),
+                rx.text(
+                    "Illness",
+                    as_="div",
+                    size="2",
+                    mb="1",
+                    weight="bold",
+                ),
+                rx.input(placeholder="Illness", on_blur=State.set_illness),
+                rx.text(
+                    "Language",
+                    as_="div",
+                    size="2",
+                    mb="1",
+                    weight="bold",
+                ),
+                rx.input(placeholder="Language", on_blur=State.set_language),
+                direction="column",
+                spacing="3",
+            ),
+            rx.flex(
+                rx.dialog.close(
+                    rx.button(
+                        "Cancel",
+                        variant="soft",
+                        color_scheme="gray",
+                    ),
+                ),
+                rx.dialog.close(
+                    rx.button(
+                        "Submit user",
+                        on_click=State.add_user,
+                        variant="solid",
+                    ),
+                ),
+                padding_top="1em",
+                spacing="3",
+                mt="4",
+                justify="end",
+            ),
+            style={"max_width": 450},
+            box_shadow="lg",
+            padding="1em",
+            border_radius="25px",
+            font_family="Inter",
+        ),
+    )
+
+def navbar():
+    return rx.hstack(
+        rx.vstack(
+            rx.heading("User Data App", size="7", font_family="Inter"),
+        ),
+        rx.spacer(),
+        add_user(),
+        rx.avatar(fallback="TG", size="4"),
+        rx.color_mode.button(rx.color_mode.icon(), size="3", float="right"),
+        position="fixed",
+        width="100%",
+        top="0px",
+        z_index="1000",
+        padding_x="4em",
+        padding_top="2em",
+        padding_bottom="1em",
+        backdrop_filter="blur(10px)",
+    )
+
+def content():
+    return rx.fragment(
+        rx.vstack(
+            rx.divider(),
+            rx.hstack(
+                rx.heading(
+                    f"Total: {State.num_users} Users",
+                    size="5",
+                    font_family="Inter",
+                ),
+                rx.spacer(),
+                rx.select(
+                    ["username", "illness"],
+                    placeholder="Sort By: Username",
+                    size="3",
+                    on_change=lambda sort_value: State.sort_values(sort_value),
+                    font_family="Inter",
+                ),
+                width="100%",
+                padding_x="2em",
+                padding_top="2em",
+                padding_bottom="1em",
+            ),
+            rx.table.root(
+                rx.table.header(
+                    rx.table.row(
+                        rx.table.column_header_cell("Username"),
+                        rx.table.column_header_cell("Name"),
+                        rx.table.column_header_cell("Age"),
+                        rx.table.column_header_cell("Language"),
+                        rx.table.column_header_cell("Illness"),
+                    ),
+                ),
+                rx.table.body(rx.foreach(State.users, show_user)),
+                # variant="surface",
+                size="3",
+                width="100%",
+            ),
+        ),
+    )
+
+@template(route="/lahacks2024", title="Main")
+def index() -> rx.Component:
+    return rx.box(
+        navbar(),
+        rx.box(
+            content(),
+            margin_top="calc(50px + 2em)",
+            padding="4em",
+        ),
+        font_family="Inter",
+    )
 
 # Create the app.
 app = rx.App()
+app.add_page(
+    index,
+    on_load=State.on_load,
+    title="User Data App",
+    description="A simple app to manage user data.",
+)
