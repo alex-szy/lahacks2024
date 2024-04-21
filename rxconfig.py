a9a7429f5e78b1fde835c100b90ef4c7d738b04f
Alex Su <suziyue@gmail.com>
2024-04-21 02:06:13 -0700

    finished dashboard

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,6 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="lahacks2024",
-    db_url="postgresql://lahacks2024_user:B5Xiu05ALDWVOfAN6eJgxwpmJf3cPKnD@dpg-coi5i40l5elc73d21an0-a.oregon-postgres.render.com/lahacks2024"
+    app_name="lahacks2024"
+    # db_url="postgresql://lahacks2024_user:B5Xiu05ALDWVOfAN6eJgxwpmJf3cPKnD@dpg-coi5i40l5elc73d21an0-a.oregon-postgres.render.com/lahacks2024"
 )
