1512399ea403f33a32f0c477c5590a27a49340d9
Alex Su <suziyue@gmail.com>
2024-04-20 21:13:34 -0700

    finished login and logout with redirects

--- lahacks2024/pages/__init__.py
+++ lahacks2024/pages/__init__.py
@@ -1,3 +1,7 @@
 from .dashboard import dashboard
 from .index import index
 from .settings import settings
+from .login import login
+from .check_email import check_email
+from .logout import logout
+from .dashboard import dashboard
--- lahacks2024/pages/index.py
+++ lahacks2024/pages/index.py
@@ -2,8 +2,33 @@
 
 from lahacks2024 import styles
 from lahacks2024.templates import template
+from reflex_magic_link_auth import MagicLinkAuthState
 
 import reflex as rx
+
+class LoginState(rx.State):
+    """
+    Global state for login status.
+    """
+
+    is_logged_in: bool = False
+
+    async def check_and_redirect(self, current_route: str):
+        """
+        Check if the user is logged in.
+        """
+        authstate = await self.get_state(MagicLinkAuthState)
+        if authstate.session_is_valid and current_route != "/":
+            return rx.redirect("/")
+        elif current_route != "/login":
+            return rx.redirect("/login")
+
+    def toggle(self):
+        """
+        Toggle the login status.
+        """
+        self.is_logged_in = not self.is_logged_in
+        print(f"Logged in: {self.is_logged_in}")
 
 
 @template(route="/", title="Home")
@@ -15,4 +40,7 @@ def index() -> rx.Component:
     """
     with open("README.md", encoding="utf-8") as readme:
         content = readme.read()
-    return rx.markdown(content, component_map=styles.markdown_style)
+    return rx.vstack(
+        rx.markdown(content, component_map=styles.markdown_style),
+        rx.button("Toggle", on_click=LoginState.toggle),
+    )
